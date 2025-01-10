c9e7fffd97d27b77dd8f4937b4b5f1c38aa10f43
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-01-09 19:41:00 -0500

    images

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -141,6 +141,35 @@ export default function Lab1() {
           </tfoot>
         </table>
       </div>
+      <div id="wd-images">
+        <h4>Image tag</h4>
+        Loading an image from the internet: <br />
+        <img id="wd-starship" width="400px"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+        <br />
+        Loading a local image:
+        <br />
+        <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+      </div>
+      <div id="wd-forms">
+        <h4>Form Elements</h4>
+        <form id="wd-text-fields">
+          <h5>Text Fields</h5>
+          <label htmlFor="wd-text-fields-username">Username:</label>
+          <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+          <label htmlFor="wd-text-fields-password">Password:</label>
+          <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+          <br />
+          <label htmlFor="wd-text-fields-first-name">First name:</label>
+          <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+          <label htmlFor="wd-text-fields-last-name">Last name:</label>
+          <input type="text" placeholder="Doe"
+            value="Wonderland"
+            title="The last name"
+            id="wd-text-fields-last-name" />
+          {/* copy rest of form elements here  */}
+        </form>
+      </div>
     </div>
   );
 }
