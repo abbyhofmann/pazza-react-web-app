d319a996238662f11992259550afb0f8e6a3b88b
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-01-10 13:11:56 -0500

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Labs from "./Labs";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+
+      </div>
+    </HashRouter>
   );
 }
