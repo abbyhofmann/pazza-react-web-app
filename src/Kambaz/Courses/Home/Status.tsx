0343d7a8c632cb28e4d32d42d963b157cfad83f0
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-02-05 12:12:41 -0500

    styling home screen

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,20 +1,50 @@
+import { Button } from "react-bootstrap";
+import { BiImport, BiSolidBarChartAlt2, BiSolidMegaphone } from "react-icons/bi";
+import { FaBell, FaCheckCircle, FaHome } from "react-icons/fa";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { MdDoNotDisturbAlt } from "react-icons/md";
+
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div id="wd-course-status" style={{ width: "350px" }}>
       <h2>Course Status</h2>
-      <table>
-        <tr>
-          <button>Unpublish</button>
-          <button>Publish</button>
-        </tr>
-        <br />
-        <tr><button>Import Existing Content</button></tr>
-        <tr><button>Import from Commons</button></tr>
-        <tr><button>Choose Home Page</button></tr>
-        <tr><button>View Course Stream</button></tr>
-        <tr><button>New Announcement</button></tr>
-        <tr><button>New Analytics</button></tr>
-        <tr><button>View Course Notifications</button></tr>
-      </table>
-    </div>);
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </Button> </div>
+        <div className="w-50">
+          <Button variant="success" size="lg" className="w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish </Button> </div>
+      </div>
+      <br />
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" />
+        Import Existing Content
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" />
+        Import from Commons
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <FaHome className="me-2 fs-5" fill="" />
+        Choose Home Page
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiSolidBarChartAlt2 className="me-2 fs-5" />
+        View Course Stream
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiSolidMegaphone className="me-2 fs-5" />
+        New Announcement
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiSolidBarChartAlt2 className="me-2 fs-5" />
+        New Analytics
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <FaBell className="me-2 fs-5" />
+        View Course Notifications
+      </Button>
+    </div>
+  );
 }
