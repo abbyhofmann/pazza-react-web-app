e4ea3ec487cb987f0b5d9f7e4d06dac1c49d20be
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-02-11 16:29:05 -0500

    course navigation

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,18 +1,22 @@
+import { courses } from "../Database";
 import { FaAlignJustify } from "react-icons/fa";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home";
 import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useParams } from "react-router";
 import PeopleTable from "./People/Table";
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+
   return (
     <div id="wd-courses">
       <h2 className="">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234 </h2> <hr />
+        {course && course.name} </h2> <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
           <CourseNavigation />
