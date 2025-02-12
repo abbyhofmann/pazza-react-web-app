bbc345958510bed5a4edc05dc76615b9ade34879
Sanjana Kashyap <100091218+sanjana-kashyap@users.noreply.github.com>
2025-02-12 17:34:31 -0500

    Added Piazza Folder (#1)
    * Added Piazza Folder
    
    * deployment fix
    
    * Piazza File in Coursees
    
    * Style Ident Fixed
    
    ---------
    
    Co-authored-by: Katie Winkleblack <kmwink21@gmail.com>

---
+++ src/Kambaz/Courses/Piazza/NewPost.tsx
@@ -0,0 +1,17 @@
+
+
+
+export default function NewPostPage() {
+   return (
+
+      <div id="wd-new-post">
+
+         <div className="wd-right-page-bg">
+            <div id="wd-class-stats" className="wd-text-grey wd-font-family
+      fs-3 wd-padding-left-class" style={{ fontWeight: 500 }}>
+               Class at a Glance
+            </div>
+         </div>
+      </div>
+   );
+}
