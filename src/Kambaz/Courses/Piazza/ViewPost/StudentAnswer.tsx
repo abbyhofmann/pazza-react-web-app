865359a0bbfea8a21667305488df537e00799ae5
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-03-17 23:02:48 -0400

    endpoint outline wip

--- src/Kambaz/Courses/Piazza/ViewPost/StudentAnswer.tsx
+++ src/Kambaz/Courses/Piazza/ViewPost/StudentAnswer.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+
+interface StudentAnswerProps {
+    studentAnswerId: string;
+}
+
 // Component for displaying a student answer to a post.
-export default function StudentAnswer() {
-    // TODO - add prop for the student answer that will come from the post object
+export default function StudentAnswer(props: StudentAnswerProps) {
+    
+    // const { studentAnswerId } = props;
+    // const [studentAnswer, setStudentAnswer] = useState<String>(""); // TODO - update to Response datatype
+
+    // useEffect(() => {
+    //     /**
+    //      * Function to fetch the student answer data based on the answer's ID.
+    //      */
+    //     const fetchData = async () => {
+    //       try {
+    //         const res = await getStudentAnswerById(studentAnswerId);
+    //         setStudentAnswer(res || null);
+    //       } catch (error) {
+    //         // eslint-disable-next-line no-console
+    //         console.error('Error fetching student answer:', error);
+    //       }
+    //     };
+    
+    //     // eslint-disable-next-line no-console
+    //     fetchData().catch(e => console.log(e));
+    //   }, [studentAnswerId]);
+
     return (
-        <article data-id="i_answer" className="answer" aria-label="Student Answer">
+        <article data-id="sa_answer" className="answer" aria-label="Student Answer">
             <header className="border-bottom container-fluid">
                 <div className="row">
                     <div className="text-left pl-0 col">
@@ -15,7 +42,7 @@ export default function StudentAnswer() {
                 <div className="g-0 row">
                     <div className="col">
                         <div className="py-3 history-selection">
-                            <div id="m7h0iykfwym12r_render" data-id="renderHtmlId" className="render-html-content overflow-hidden latex_process">here is a sample student answer</div>
+                            <div id="m7h0iykfwym12r_render" data-id="renderHtmlId" className="render-html-content overflow-hidden latex_process">here is a sample student answer</div> { /* TODO - replace hard-coded content with studentAnswer.content */}
                         </div>
                     </div>
                 </div>
@@ -28,7 +55,7 @@ export default function StudentAnswer() {
                     </div>
                     <div className="text-right col">
                         { /* we don't need last updated at, but we do need the timestamp and author of who answered it */}
-                        <div className="update_text float-end" data-id="contributors">Answered on <time>February 28, 2025 at 8:11 am</time> by <span data-id="contributors">Abby Hofmann</span>
+                        <div className="update_text float-end" data-id="contributors">Answered on <time>February 28, 2025 at 8:11 am</time> by <span data-id="contributors">Abby Hofmann</span> { /* TODO - replace hard-coded content with studentAnswer.authors and studentAnswer.dateEdited */}
                         </div>
                     </div>
                 </div>
