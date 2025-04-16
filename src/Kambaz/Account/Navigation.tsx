b11da66b7810228d05c1265fc2d97523f0be6520
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-04-16 16:59:09 -0400

    edit user

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
 
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0 me-4">
@@ -16,6 +18,8 @@ export default function AccountNavigation() {
           {link}
         </Link>)
       }
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link>)}
     </div>
   );
 }
