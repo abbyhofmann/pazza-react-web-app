03a8f7c3dd8c726d4cf90d00468fe89d198f3111
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-04-21 00:20:28 -0400

    protect manage class link from non-faculty

--- src/Kambaz/Courses/Piazza/PiazzaNavBarTop.tsx
+++ src/Kambaz/Courses/Piazza/PiazzaNavBarTop.tsx
@@ -1,10 +1,14 @@
 import { Link, useParams } from "react-router-dom";
 import "./navbar.css";
+import { useSelector } from "react-redux";
 
 export default function PiazzaNavBarTop() {
 
   const { cid } = useParams();
   const path = `/Kambaz/Courses/${cid}/Piazza`;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const isFaculty = currentUser.role === "FACULTY";
 
   return (
     <nav className="navbar navbar-expand-lg wd-border-piazza-nav-bar d-flex">
@@ -29,16 +33,17 @@ export default function PiazzaNavBarTop() {
           <li className="nav-item">
             <a className="nav-link">Statistics</a>
           </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to={path + "/manage-class"}
-              id={`wd-manage-class-link`}
-            >
-              Manage Class
-            </Link>
-            {/* TODO: protect this button */}
-          </li>
+          {isFaculty &&
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to={path + "/manage-class"}
+                id={`wd-manage-class-link`}
+              >
+                Manage Class
+              </Link>
+            </li>
+          }
         </ul>
       </div>
     </nav>
