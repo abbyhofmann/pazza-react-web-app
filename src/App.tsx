5e3bbd068b253ed0b01c0369f953ddd54c3e9e3a
Katie <katiewinkleblack>
2025-04-21 10:53:13 -0400

    errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import Labs from "./Labs";
 import Kambaz from "./Kambaz";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import store from "./Kambaz/store";
-import { Provider } from "react-redux";
 export default function App() {
   return (
     <HashRouter>
-      <Provider store={store}>
-        <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="Kambaz" />} />
-            <Route path="/Labs/*" element={<Labs />} />
-            <Route path="/Kambaz/*" element={<Kambaz />} />
-          </Routes>
-        </div>
-      </Provider>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kambaz" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
+
+      </div>
     </HashRouter>
   );
 }
