62eacd7d64ecde5598df8086d96a2630e98a69c5
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-04-21 00:58:08 -0400

    landing page

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -18,16 +18,29 @@ export default function Signin() {
   };
 
   return (
-    <div id="wd-signin-screen">
-      <h1>Sign in</h1>
-      <FormControl defaultValue={credentials.username}
-        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-        className="mb-2" placeholder="username" id="wd-username" />
-      <FormControl defaultValue={credentials.password}
-        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-        className="mb-2" placeholder="password" type="password" id="wd-password" />
-      <Button onClick={signin} id="wd-signin-btn" className="w-100"> Sign in </Button>
-      <Link id="wd-signup-link" to="/Kambaz/Account/Signup"> Sign up </Link>
+    <div id="wd-signin-screen" className="d-flex w-100">
+      <div className="w-100">
+        <h1>Sign in</h1>
+        <FormControl defaultValue={credentials.username}
+          onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+          className="mb-2" placeholder="username" id="wd-username" />
+        <FormControl defaultValue={credentials.password}
+          onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+          className="mb-2" placeholder="password" type="password" id="wd-password" />
+        <Button onClick={signin} id="wd-signin-btn" className="w-100"> Sign in </Button>
+        <Link id="wd-signup-link" to="/Kambaz/Account/Signup"> Sign up </Link>
+      </div>
+      <div className="d-flex gap-3 ms-auto">
+        <div className="flex-fill">Team Pazza - ASK</div>
+        <div className="d-flex flex-column gap-3">
+          <div className="flex-fill">Abby Hofmann</div>
+          <div className="flex-fill">Sanjana Kashyap</div>
+          <div className="flex-fill">Katie Winkleblack</div>
+        </div>
+        <Link className="flex-fill" to={"https://github.com/abbyhofmann/pazza-react-web-app"}>
+          Github Repository
+        </Link>
+      </div>
     </div>
   );
 }
